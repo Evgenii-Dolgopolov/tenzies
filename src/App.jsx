7b36f9cbf39b9a00a083bb57241e54bc2cec9219
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-03-06 13:03:27 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from "react"
+import "./App.css"
+import Die from "./components/die"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+
+export default function App() {
+  const [dice, setDice] = useState(allNewDice())
+  const [tenzies, setTenzies] = useState(false)
+
+  useEffect(() => {
+    if (dice.every(die => die.isHeld && die.value === dice[0].value)) {
+      setTenzies(true)
+    }
+  }, [dice])
+
+  function generateNewDie() {
+    return {
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }
+  }
+
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+      newDice.push(generateNewDie())
+    }
+    return newDice
+  }
+
+  const diceElements = dice.map(die => (
+    <Die
+      key={die.id}
+      value={die.value}
+      isHeld={die.isHeld}
+      id={die.id}
+      holdDice={holdDice}
+    />
+  ))
+
+  function rollDice() {
+    if (!tenzies) {
+      setDice(oldDice =>
+        oldDice.map(die => {
+          return die.isHeld ? die : generateNewDie()
+        })
+      )
+    } else {
+      setTenzies(false)
+      setDice(allNewDice())
+    }
+  }
+
+  function holdDice(id) {
+    const dieId = dice.find(die => die.id === id)
+    if (dieId) {
+      setDice(
+        dice.map(die => {
+          return die.id === id ? { ...die, isHeld: !die.isHeld } : die
+        })
+      )
+    } else {
+      console.log(`No dice found with ID ${id}`)
+    }
+  }
+
+  return (
+    <main>
+      {tenzies && <Confetti />}
+      <h1 className="title">Tenzies</h1>
+      <p className="instructions">
+        Roll until all dice are the same. Click each die to freeze it at its
+        current value between rolls.
+      </p>
+      <div className="dice-container">{diceElements}</div>
+      <button className="roll-dice" onClick={rollDice}>
+        {tenzies ? "New Game" : "Roll"}
+      </button>
+    </main>
+  )
+}
